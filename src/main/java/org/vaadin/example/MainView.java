a2b3e3c190936429c153f8390dcdf5e677e0210a
Archie L. Cobbs <archie.cobbs@gmail.com>
2024-02-08 13:16:45 -0600

    Demonstrate Grid loading bug.

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -3,11 +3,17 @@ package org.vaadin.example;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * The main view contains a text field for getting the user name and a button
@@ -17,27 +23,26 @@ import com.vaadin.flow.router.Route;
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
-        textField.addClassName("bordered");
-        // Button click listeners can be defined as lambda expressions
-        GreetService greetService = new GreetService();
-        Button button = new Button("Say hello", e -> {
-            add(new Paragraph(greetService.greet(textField.getValue())));
-        });
-
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button is more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
-
-        // Use custom CSS classes to apply styling. This is defined in
-        // styles.css.
-        addClassName("centered-content");
-
-        add(textField, button);
+        Grid<String> grid = new Grid<>(String.class);
+        grid.addColumn(String::toString)
+          .setKey("value")
+          .setWidth("200px")
+          .setHeader("Value");
+        ListDataProvider<String> dp = new ListDataProvider<>(new ArrayList<>());
+        grid.setItems(dp);
+        add(grid);
+        VaadinSession session = VaadinSession.getCurrent();
+        new Thread(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+            session.access(() -> {
+                dp.getItems().add("String #1");
+                dp.getItems().add("String #2");
+                dp.getItems().add("String #3");
+                dp.refreshAll();
+            });
+        }).start();
     }
 }
